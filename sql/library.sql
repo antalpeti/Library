2e4d78d26e5e631f243c44aa420ca8a94c016daf
Peti <antalpeti@users.noreply.github.com>
2015-07-10 02:08:49 +0200

    Replace database creation script with create structure and insert data script.

--- sql/library.sql
+++ sql/library.sql
@@ -1,55 +1,84 @@
--- MySQL Workbench Forward Engineering
+CREATE DATABASE  IF NOT EXISTS `library` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `library`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: library
+-- ------------------------------------------------------
+-- Server version	5.6.23-log
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema library
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `library` ;
+--
+-- Table structure for table `book`
+--
 
--- -----------------------------------------------------
--- Schema library
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `library` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `library` ;
-
--- -----------------------------------------------------
--- Table `library`.`member`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `library`.`member` ;
-
-CREATE TABLE IF NOT EXISTS `library`.`member` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL,
+  `author` varchar(45) NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `pages` int(11) NOT NULL,
+  `memberid` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB;
+  KEY `memberid_idx` (`memberid`),
+  CONSTRAINT `memberid` FOREIGN KEY (`memberid`) REFERENCES `member` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `book`
+--
 
--- -----------------------------------------------------
--- Table `library`.`book`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `library`.`book` ;
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (1,'J. K. Rowling','Harry Potter and the Philosopher\'s Stone',223,NULL),(2,'J. K. Rowling','Harry Potter and the Chamber of Secrets',251,NULL),(3,'J. K. Rowling','Harry Potter and the Prisoner of Azkaban',317,NULL),(4,'J. K. Rowling','Harry Potter and the Goblet of Fire',636,NULL),(5,'J. K. Rowling','Harry Potter and the Order of the Phoenix',766,NULL),(6,'J. K. Rowling','Harry Potter and the Half-Blood Prince',607,NULL),(7,'J. K. Rowling','Harry Potter and the Deathly Hallows',607,NULL),(8,'George R. R. Martin','A Game of Thrones',864,NULL),(9,'George R. R. Martin','A Clash of Kings',931,NULL),(10,'George R. R. Martin','A Storm of Swords',625,NULL),(11,'George R. R. Martin','A Feast for Crows',865,NULL),(12,'George R. R. Martin','A Dance with Dragons',1154,NULL);
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `library`.`book` (
-  `id` INT NOT NULL,
-  `author` VARCHAR(45) NOT NULL,
-  `title` VARCHAR(45) NOT NULL,
-  `pages` INT NOT NULL,
-  `memberid` INT NULL,
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `member` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `memberid_idx` (`memberid` ASC),
-  CONSTRAINT `memberid`
-    FOREIGN KEY (`memberid`)
-    REFERENCES `library`.`member` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `member`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (1,'peti','peti@gmail.com'),(2,'balazs','balazs@gmail.com');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-07-10  1:40:30
